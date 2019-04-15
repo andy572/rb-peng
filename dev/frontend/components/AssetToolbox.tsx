f09d766e1f31e33b1c87939c549e50b0456af918
Andreas Bratfisch <bratfisch@redblue.de>
2019-04-15 09:13:33 +0200

    Don't show element, if no products are available

--- dev/frontend/components/AssetToolbox.tsx
+++ dev/frontend/components/AssetToolbox.tsx
@@ -2,16 +2,32 @@ import * as React from "react";
 import {ResultSelectionView} from "./ResultSelectionView";
 import {FlexContainer} from "./core/FlexContainer";
 import {Typography} from "./core/Typography";
+import {ChildDataProps, Query} from "react-apollo";
+import gql from "graphql-tag";
 
 type AssetToolboxProps = {
-    client: any
+    client: any,
+    data?: any
 }
 
-export class AssetToolbox extends React.Component<AssetToolboxProps> {
+const GET_CACHED_PRODUCTS_QUERY = gql`
+query ProductList {
+    products @client {
+        articleNumber
+    }
+}
+`;
+
+export class AssetToolbox extends React.Component<AssetToolboxProps & ChildDataProps> {
     render() {
-        return <FlexContainer direction={"column"} className={"rb-page-content rb-right-panel"}>
-            <Typography variant={"h2"}>Asset-Aktionen</Typography>
-            <ResultSelectionView client={this.props.client}/>
-        </FlexContainer>
+        return <Query query={GET_CACHED_PRODUCTS_QUERY}>
+            {({ loading, error, data }) => {
+                if (!data || data.products.length == 0) return null;
+                return <FlexContainer direction={"column"} className={"rb-page-content rb-right-panel"}>
+                    <Typography variant={"h2"}>Asset-Aktionen</Typography>
+                    <ResultSelectionView client={this.props.client}/>
+                </FlexContainer>
+            }}
+        </Query>
     }
 }
